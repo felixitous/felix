f77a7e2930a01d77797101861d90b096b2de9211
Felix Liu <felixliu@berkeley.edu>
2015-03-27 18:31:33 -0700

    start

---
+++ felix/wsgi.py
@@ -0,0 +1,14 @@
+import os
+from dj_static import Cling
+"""
+WSGI config for felix project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "felix.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = Cling(get_wsgi_application())
